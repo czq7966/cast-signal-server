ca99a82d915d9132714ef08f58542973e4bedce9
陈志谦 <761208@nd.com>
2020-03-18 21:56:50 +0800

    Page common ipc-connection constructor add conn params

--- src/desktop/receiver/web/pages/common/ipc-connection.ts
+++ src/desktop/receiver/web/pages/common/ipc-connection.ts
@@ -10,16 +10,18 @@ export interface IIPCConnection extends ADHOCCAST.Cmds.Common.IBase {
 export class IPCConnection extends ADHOCCAST.Cmds.Common.Base implements IIPCConnection {
     eventRooter: ADHOCCAST.Cmds.Common.IEventRooter;
     connection: Common.Modules.IIPCConnection;
-    constructor(params?: ADHOCCAST.Cmds.Common.IBaseConstructorParams) {
+    constructor(params?: ADHOCCAST.Cmds.Common.IBaseConstructorParams,
+                connParams?: ADHOCCAST.IConnectionConstructorParams) {
         super(params);
-        this.eventRooter = new ADHOCCAST.Cmds.Common.EventRooter();
-        let connParams: ADHOCCAST.IConnectionConstructorParams = {
+        this.eventRooter = new ADHOCCAST.Cmds.Common.EventRooter();        
+        let _connParams: ADHOCCAST.IConnectionConstructorParams = {
             signalerBase: "",
             namespace: "",
             instanceId: this.instanceId,
             factorySignaler: Common.Modules.IPCRenderSignaler.TAG,
             parent: this    
         }
+        connParams = Object.assign(_connParams, connParams)
 
         this.connection = Common.Modules.IPCConnection.getInstance(connParams); 
         this.eventRooter.setParent(this.connection.dispatcher.eventRooter);
